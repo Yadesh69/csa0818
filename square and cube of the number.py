78394baaf1ce9509ed196f0724fb007b26561911
Yadesh <192511306.simats@saveetha.com>
2025-10-28 09:47:56 +0530

    Add files via upload

---
+++ square and cube of the number.py
@@ -0,0 +1,6 @@
+#To find the square and cube of the number
+a=int(input("Enter a number:"))
+square=a**2
+cube=a**3
+print("The square of the number is:",square)
+print("The cube of the number is:",cube)
